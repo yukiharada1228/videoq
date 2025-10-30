e7587d20fc042d9c831252390245fa4d7dbce119
yukiharada1228 <yukiharada1228@gmail.com>
2025-10-30 10:23:51 +0900

    Update access token lifetime to 1 minute and enable refresh token rotation

--- backend/ask_video/settings.py
+++ backend/ask_video/settings.py
@@ -170,9 +170,9 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
-    "ROTATE_REFRESH_TOKENS": False,
+    "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
