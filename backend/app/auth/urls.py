404a24e2b724559e494bcdd9f35b3cd94eb8b777
yukiharada1228 <yukiharada1228@gmail.com>
2025-10-31 08:57:13 +0900

    Enable signup endpoint in auth URLs

--- backend/app/auth/urls.py
+++ backend/app/auth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import LoginView, LogoutView, MeView, RefreshView, UserSignupView
 
 urlpatterns = [
-    # path("signup/", UserSignupView.as_view(), name="signup"),
+    path("signup/", UserSignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="auth-login"),
     path("logout/", LogoutView.as_view(), name="auth-logout"),
     path("refresh/", RefreshView.as_view(), name="auth-refresh"),
