642f53f12ba83a78329df14c1a1330e5f19ed4a3
yukiharada1228 <yukiharada1228@gmail.com>
2025-12-12 15:26:12 +0900

    chore: DEBUG設定をFalseに戻す

--- backend/videoq/settings.py
+++ backend/videoq/settings.py
@@ -68,7 +68,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", DefaultSettings.SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
